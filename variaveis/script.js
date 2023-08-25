fb25a59a4d94ee51b02a929887c7d3c15218b0a5
mandarpitkar <139145085+mandarpitkar@users.noreply.github.com>
2023-08-25 20:17:47 -0300

    exercicio variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,28 @@
+// var nome = 'Mandar'
+// var idade = 25
+// var comida = "pizza"
+// console.log(nome, idade, comida)
+
+// let nome ='Mandar'
+// idade = 25
+// comida = "Pizza"
+// console.log(nome, idade, comida)
+
+
+console.log('const') 
+
+var nome
+var idade
+var sexo
+var profession
+var cidade
+var pais
+
+    // nome = 'Mandar Pitkar'
+    // idade = 30 
+    // sexo = ' Sexo Masculino'
+    // profession = "TI"
+    // cidade = "Sao Paulo"
+    // pais = 'Brasil'
+
+console.log(nome,idade,sexo,profession,cidade,pais)
