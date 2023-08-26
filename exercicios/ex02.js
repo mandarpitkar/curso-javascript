0d634294a9d8eff6f8e835036f24b16ae94dcfd9
mandarpitkar <139145085+mandarpitkar@users.noreply.github.com>
2023-08-26 10:46:09 -0300

    ex02--Strings

---
+++ exercicios/ex02.js
@@ -0,0 +1,30 @@
+//Declare uma var contendo um string
+var nome = 'Mandar'
+console.log(typeof nome)
+
+//Declare uma var contendo um numero dentro do string
+
+var numero = '25'
+console.log(typeof numero)
+
+//Declare uma var com a sua idade
+var idade = 36
+console.log(typeof idade)
+
+//Declare duas var, uma com seu nome e outra com sobrenome e some os mesmos
+
+var nome = 'Mandar'
+var sobrenome = 'Pitkar'
+console.log(nome + ' ' + sobrenome)
+console.log (`${nome} ${sobrenome}`)
+
+//Coloque o seguinte frase em uma var: It's time
+console.log('It\'s time')
+console.log(`It's time`)
+
+var time = 10
+console.log(`It's ${time} in the morning.`)
+
+//Verifique o tipo do var que contem seu nome
+var verificarTipoNome = typeof nome
+console.log (verificarTipoNome)
