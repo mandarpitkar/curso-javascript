20f424138565e4cab5e3c0195573332cd4253b69
mandarpitkar <139145085+mandarpitkar@users.noreply.github.com>
2023-08-26 13:02:35 -0300

    ex03.js
    Operadores Aritmeticos

---
+++ exercicios/ex03.js
@@ -0,0 +1,29 @@
+//Qual o resultado da seguinte expressao?
+var total = 10 + 5 * 2 / 2 + 20 // 35
+console.log(total)
+
+//Crie duas expressao que retornar NaN
+var x = '10s'
+var somax = 5
+console.log(+x + somax)
+
+var y = 20
+var somay = '10s'
+console.log(+somay + ++somax)
+
+//Somar a string '200' com o numero 50 e retornar 250
+var n1 = '200'
+var n2 = 50
+console.log(+n1 + n2)
+
+//Incremente o numero 5 e retorno seu valor incrementado
+var n = 5
+console.log(n++)
+console.log(n)
+console.log(++n)
+
+//Como dividir o peso por 2?
+var num = + '80' / 2
+var unidade = 'kg'
+var peso = num + unidade
+console.log(peso)
