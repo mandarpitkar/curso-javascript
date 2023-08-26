499288d40e58c40ce3bfafde53b0a00ff6a5feec
mandarpitkar <139145085+mandarpitkar@users.noreply.github.com>
2023-08-26 10:47:04 -0300

    Tipo de dados
    Strings(Aspas, Duplas, Simples e Teemplates

---
+++ tipo de dados/script.js
@@ -0,0 +1,60 @@
+
+
+var nome = 'Mandar' //string
+var sobrenome = "Pitkar" // string
+var idade = 36 // number
+var masculino = true //ou false// boolean
+var time //undefined 
+var nome = null // object
+var nome = [1, 2 , "Mandar"] // object
+
+
+ 
+var nome = 'Mandar'
+var sobrenome = "Pitkar"
+var nomeCompleto = nome + " " + sobrenome
+console.log(nomeCompleto)
+
+
+
+var data = '26 '
+var mes = "8"
+var anoatual = 2023
+var anonasci = 1987
+var idade = anoatual - anonasci
+
+console.log(  data + mes + ' ' +idade )
+
+
+
+// Aspas, Duplas & Simples: Strings
+var frase1 = 'Mandar eh "super" homen' // valido
+console.log(frase1)
+var frase2 = "Mandar eh 'super' homen" // valido
+console.log(frase2)
+//var frase3 = 'Mandar eh 'super' homen' // invalido
+//console.log(frase3)
+var frase4 = 'Mandar eh \'super\' homen' // valido
+console.log(frase4)
+var frase5 = `"Mandar eh (super) homen"` // valido
+console.log(frase5)
+
+
+//Template String
+var nome = 'Mandar'
+var gol = 10
+var res = 
+console.log(`${nome} fez ${gol} gols`)
+
+// Template String
+var num1 = 5
+var num2 = 6
+console.log(`A soma entre ${num1} + ${num2} eh igual ${num1+num2}`)
+
+
+
+
+
+
+
+
